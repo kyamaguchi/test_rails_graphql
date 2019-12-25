d30036c1454d4e061cab8fd5025791e37763c126
kyamaguchi <kzh.yap@gmail.com>
2019-12-25 10:06:51 +0900

    Fetch and render Users
    Got the error "HTTP parse error, malformed request (): #<Puma::HttpParserError: Invalid HTTP format, parsing fails.>"
    when createHttpLink has https.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,12 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Users from './Users';
+class App extends Component {
+  render() {
+    return (
+      <div className="container mx-auto px-4">
+        <Users />
+      </div>
+    );
+  }
 }
-
 export default App;
